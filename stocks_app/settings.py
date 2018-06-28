801fec4e85ea4cf3c3eab41d79936df02809406f
mayankrasu <mayank.rasu@gmail.com>
2018-06-29 00:37:37 +0530

    added google analytics

--- stocks_app/settings.py
+++ stocks_app/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'django-google-analytics',
     'stocks_app',
 
 ]
